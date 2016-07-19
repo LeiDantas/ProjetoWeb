d3fd2df530ba276f38b7bb611c92120d621a7657
Leandro Dantas <leandrobdantas@outlook.com>
2016-07-18 23:28:58 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE>
+<html lang="pt-BR">
+<head>
+    <meta charset="utf-8">
+    <title>ProjectLD - Candidatura estágio</title>
+    
+    <!-- Inicializando bootstrap direto do website-->
+    <link rel="stylesheet" href= "https://maxcdn.Bootstrapcdn.com/Bootstrap/3.3.2/css/Bootstrap.min.css" > 
+    <link rel="stylesheet" href= "https://maxcdn.Bootstrapcdn.com/Bootstrap/3.3.2/css/Bootstrap-theme.min.css" >
+    
+    <link rel=stylesheet href="../css/reset.css"> <!-- Reset Eric Meyer, para facilitar -->
+    
+</head>
+<body>
+    
+    <h1>Teste</h1>
+    
+    <p>Fundada há <?php print date("Y") - 1932; ?> anos.</p>
+    <h2>Teste2</h2>
+    <!-- Dependência jQuery-->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script src= "https://maxcdn.Bootstrapcdn.com/Bootstrap/3.3.2/js/Bootstrap.min.js" ></script>    
+</body>
+</html>
